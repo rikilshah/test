5e0abf8431fde1c0da5d5789f3efc05dbfd61171
Rikil Shah <rikil.shah@gmail.com>
2019-12-20 13:18:19 +0530

    First Code

---
+++ simpleToggle/simpleToggle/Debug/simpleToggle.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\bronco\AppData\Local\Temp\VMBuilds\simpleToggle\mega_atmega2560\Debug\simpleToggle.cpp.o: \
+ C:\Users\bronco\AppData\Local\Temp\VMBuilds\simpleToggle\mega_atmega2560\Debug\simpleToggle.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h
---
+++ simpleToggle/simpleToggle/simpleToggle.ino
@@ -0,0 +1,20 @@
+/*
+ * simpleToggle.ino
+ *
+ * Created: 12/20/2019 1:17:33 PM
+ * Author: bronco
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
